33fe7b3568902a3a7d22c73fcbc7f7c5a448a124
Yannis699 <yannis.cristini@gmail.com>
2022-05-19 22:38:37 +0200

    ajout fonctionnalité back pour remplir le formulaire permettant de créer un article

--- blog/add-article.php
+++ blog/add-article.php
@@ -1,3 +1,73 @@
+<?php
+const ERROR_REQUIRED = 'Veuillez renseigner ce champ';
+const ERROR_TITLE_TOO_SHORT = 'Le titre est trop court';
+const ERROR_CONTENT_TOO_SHORT = "L'article est trop court";
+const ERROR_IMAGE_URL = "L'image doit être une URL valide";
+$filename = __DIR__ . '/data/articles.json';
+$errors = [
+  'title' => '',
+  'image' => '',
+  'category' => '',
+  'content' => '',
+];
+
+$articles = [];
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (file_exists($filename)) {
+        $articles = json_decode(file_get_contents($filename), true) ?? [];
+      }  $_POST = filter_input_array(INPUT_POST, [
+    'title' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+    'image' => FILTER_SANITIZE_URL,
+    'category' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+    'content' => [
+      'filter' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+      'flags' => FILTER_FLAG_NO_ENCODE_QUOTES
+    ]
+  ]);
+  $title = $_POST['title'] ?? '';
+  $image = $_POST['image'] ?? '';
+  $category = $_POST['category'] ?? '';
+  $content = $_POST['content'] ?? '';
+
+  if (!$title) {
+    $errors['title'] = ERROR_REQUIRED;
+  } elseif (mb_strlen($title) < 5) {
+    $errors['title'] = ERROR_TITLE_TOO_SHORT;
+  }
+
+  if (!$image) {
+    $errors['image'] = ERROR_REQUIRED;
+  } elseif (!filter_var($image, FILTER_VALIDATE_URL)) {
+    $errors['image'] = ERROR_IMAGE_URL;
+  }
+
+  if (!$category) {
+    $errors['category'] = ERROR_REQUIRED;
+  }
+
+  if (!$content) {
+    $errors['content'] = ERROR_REQUIRED;
+  } elseif (mb_strlen($content) < 50) {
+    $errors['content'] = ERROR_CONTENT_TOO_SHORT;
+  }
+
+  if (empty(array_filter($errors, fn ($e) => $e !== ''))) {
+    $articles = [...$articles, [
+      'title' => $title,
+      'image' => $image,
+      'category' => $category,
+      'content' => $content,
+      'id' => time()
+    ]];
+
+    file_put_contents($filename, json_encode($articles));
+    header('Location: /');
+  }
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -40,7 +110,7 @@
               <!-- <p class="text-error"></p> -->
             </div>
             <div class="form-actions">
-              <a href="/" class="btn btn-secondary" type="button">Annuler</a>
+              <a href="/" class="btn btn-secondary" type="submit">Annuler</a>
               <button class="btn btn-primary" type="submit">Sauvegarder</button>
             </div>
           </form>
