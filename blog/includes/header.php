b15caec1a63c25c410213f469a4c13310ecf8fe7
Yannis699 <yannis.cristini@gmail.com>
2022-05-19 21:30:26 +0200

    ajout du fichier .gitignore

--- blog/includes/header.php
+++ blog/includes/header.php
@@ -2,6 +2,7 @@
 <header>
    <a href="/" class="logo">Blog</a>
    <ul class="header-menu">
+       <!-- Utilisation de l'URI pour afficher une classe -->
        <li class="<?= $_SERVER['REQUEST_URI'] ==='/add-article.php' ? 'active' : '' ?>">
            <a href="/add-article.php"> Écrire un article </a>
        </li>
