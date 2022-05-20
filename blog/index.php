c75f0b225313cb80d1247019e90d5e135e23b32a
Yannis699 <yannis.cristini@gmail.com>
2022-05-20 22:33:17 +0200

    ajout de l'affichage des articles sans la catégorie et la description

--- blog/index.php
+++ blog/index.php
@@ -1,23 +1,41 @@
+<?php
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
+$categories = [];
 
+if (file_exists($filename)) {
+  $articles = json_decode(file_get_contents($filename), true) ?? [];
+    $categories = array_map(fn($a) => $a['category'], $articles);
+}
+
+
+?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
+  <head>
+      <?php require_once 'includes/head.php' ?>
+      <link rel="stylesheet" href="/public/css/index.css">
+      <title>Blog</title>
+  </head>
 
-<head>
-    <?php require_once 'includes/head.php' ?>
-    <link rel="stylesheet" href="public/css/index.css">
-    <title>Blog</title>
-</head>
-
-<body>
+  <body>
     <div class="container">
-        <?php require_once 'includes/header.php' ?>
-        <div class="content">
-
+      <?php require_once 'includes/header.php' ?>
+      <div class="content">
+        <div class="articles-container">
+          <?php foreach ($articles as $a) : ?>
+            <div class="article block">
+              <div class="overflow">
+                <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+              </div>
+              <h2><?= $a['title'] ?></h2>
+            </div>
+          <?php endforeach; ?>
         </div>
-        <?php require_once 'includes/footer.php' ?>
+      </div>
+      <?php require_once 'includes/footer.php' ?>
     </div>
 
-</body>
-
+  </body>
 </html>
