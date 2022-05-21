3e85b0deb3ca01587794d70649645722f2d849b0
Yannis699 <yannis.cristini@gmail.com>
2022-05-21 22:34:11 +0200

    ajout des liens pour avoir la vue de détail des articles

--- blog/show-article.php
+++ blog/show-article.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/show-article.css">
+    <link rel="stylesheet" href="/public/css/style.css">
     <title>Article</title>
 </head>
 <body>
     <div class="container">
         <?php require_once 'includes/header.php'; ?>
         <div class="content">
-        <?php require_once 'includes/footer.php'; ?>
         </div>
+        <?php require_once 'includes/footer.php'; ?>
     </div>
 </body>
 </html>
