46f14deedd0d657e2a63cf4cb66016acafda7e18
Yannis699 <yannis.cristini@gmail.com>
2022-05-21 21:58:19 +0200

    affichage de la catégorie

--- blog/index.php
+++ blog/index.php
@@ -5,37 +5,56 @@ $categories = [];
 
 if (file_exists($filename)) {
   $articles = json_decode(file_get_contents($filename), true) ?? [];
-    $categories = array_map(fn($a) => $a['category'], $articles);
+  $cattmp = array_map(fn ($a) => $a['category'],  $articles);
+  $categories = array_reduce($cattmp, function ($acc, $cat) {
+    if (isset($acc[$cat])) {
+      $acc[$cat]++;
+    } else {
+      $acc[$cat] = 1;
+    }
+    return $acc;
+  }, []);
+  $articlePerCategories = array_reduce($articles, function ($acc, $article) {
+    if (isset($acc[$article['category']])) {
+      $acc[$article['category']] = [...$acc[$article['category']], $article];
+    } else {
+      $acc[$article['category']] = [$article];
+    }
+    return $acc;
+  }, []);
 }
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-      <?php require_once 'includes/head.php' ?>
-      <link rel="stylesheet" href="/public/css/index.css">
-      <title>Blog</title>
-  </head>
+    <head>
+        <?php require_once 'includes/head.php' ?>
+        <link rel="stylesheet" href="/public/css/index.css">
+        <title>Blog</title>
+    </head>
 
-  <body>
-    <div class="container">
-      <?php require_once 'includes/header.php' ?>
-      <div class="content">
-        <div class="articles-container">
-          <?php foreach ($articles as $a) : ?>
-            <div class="article block">
-              <div class="overflow">
-                <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
-              </div>
-              <h2><?= $a['title'] ?></h2>
+    <body>
+        <div class="container">
+            <?php require_once 'includes/header.php' ?>
+            <div class="content">
+                  <div class="category-container">
+                    <?php foreach ($categories as $cat => $num) : ?>
+                        <h2><?= $cat ?></h2>
+                        <div class="articles-container">
+                            <?php foreach ($articlePerCategories[$cat] as $a) : ?>
+                                <div class="article block">
+                                    <div class="overflow">
+                                        <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                                    </div>
+                                    <h3><?= $a['title'] ?></h3>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
             </div>
-          <?php endforeach; ?>
+            <?php require_once 'includes/footer.php' ?>
         </div>
-      </div>
-      <?php require_once 'includes/footer.php' ?>
-    </div>
 
-  </body>
+    </body>
 </html>
